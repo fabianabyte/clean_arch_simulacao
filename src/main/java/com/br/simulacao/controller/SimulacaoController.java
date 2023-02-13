5aa042c0d4d7e231148480a6fac95cb95ccb6784
Fabiana Alencar <fabianabyte@gmail.com>
2023-02-13 19:11:58 -0300

    Ajustes para apreseção do tipo CA-layers-single-module - Camadas em um único módulo

--- src/main/java/com/br/simulacao/controller/SimulacaoController.java
+++ src/main/java/com/br/simulacao/controller/SimulacaoController.java
@@ -2,12 +2,8 @@ package com.br.simulacao.controller;
 
 
 import com.br.simulacao.domain.model.api.simulacao.Simulacao;
-import com.br.simulacao.service.SimulacaoService;
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,48 +16,19 @@ import java.util.List;
 @RequestMapping("/simulacoes")
 public class SimulacaoController {
 
-    @NonNull
-    SimulacaoService simulacaoService;
 
     @GetMapping("/")
     public ResponseEntity<List<Simulacao>> obterTodasSimulacoes() {
-        try {
-            List<Simulacao> simulacoes = simulacaoService.obterTodasSimulacoes();
-
-            if (simulacoes.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-
-            return new ResponseEntity<>(simulacoes, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.toString());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/{id_pessoa}")
     public ResponseEntity<List<Simulacao>> obterSimulacoesPessoa(@PathVariable(name = "id_pessoa") String idPessoa) {
-        try {
-            List<Simulacao> simulacoes = simulacaoService.obterSimulacoesPorPessoa(idPessoa);
-
-            if (simulacoes.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-
-            return new ResponseEntity<>(simulacoes, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error(e.toString());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/")
     public ResponseEntity<Simulacao> criarSimulacao(@RequestBody Simulacao simulacao) {
-        try {
-            return new ResponseEntity<>(simulacaoService.criarSimulacao(simulacao), HttpStatus.CREATED);
-        } catch (Exception e) {
-            log.error(e.toString());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }

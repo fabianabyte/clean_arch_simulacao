d31fb9635737a805654127bfa0e138bbc07cd357
Fabiana Alencar <fabianabyte@gmail.com>
2023-02-14 09:31:46 -0300

    correção de arquivos faltantes

---
+++ _veiculos/src/main/java/com/br/domain/src/main/java/com/br/simulacao/veiculos/model/api/simulacao/Produto.java
@@ -0,0 +1,15 @@
+package com.br.simulacao.model.api.simulacao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Produto {
+    private String nome;
+    private TipoProduto tipoProduto;
+}

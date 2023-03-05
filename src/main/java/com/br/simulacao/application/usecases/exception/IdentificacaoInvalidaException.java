af74fae097b3a003029a309588fb0004e97ea335
Fabiana Alencar <fabianabyte@gmail.com>
2023-03-05 07:20:17 -0300

    Refactorgin da utilização de clean archi no projeto.

---
+++ src/main/java/com/br/simulacao/application/usecases/exception/IdentificacaoInvalidaException.java
@@ -0,0 +1,7 @@
+package com.br.simulacao.application.usecases.exception;
+
+public class IdentificacaoInvalidaException extends Exception{
+    public IdentificacaoInvalidaException(String excecao){
+        super(excecao);
+    }
+}
